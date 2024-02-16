de8c68be08106e79e76dec9b5e2f5962473279fc
tejag31 <152838751+tejag31@users.noreply.github.com>
2024-02-16 21:47:05 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import './Card.Js';
+
+function App() {
+  return (
+ <div>
+  <Card></Card>
+  <h3>Best Food in Hyderabad</h3>
+  <img link={"https://b.zmtcdn.com/data/pictures/chains/1/90391/5bc645dbf2bed815eb65c8e5f1d7b5ea_o2_featured_v2.jpg?output-format=webp"}></img>
+ </div>
+
+  );
+}
+
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card({imglink}) {
+  return (
+    <div className="website">
+
+   <p>hotel rumaan</p>
+   <span>4.1*</span>
+    </div>
+  )
+}
+
+export default Card;
